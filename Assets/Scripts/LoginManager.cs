8b79be42317d428d07054a78c91b922e9de47394
Roh <82229769+rohyunsang@users.noreply.github.com>
2023-05-25 15:43:06 +0900

    Create LoginManager.cs

---
+++ Assets/Scripts/LoginManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviour
+{
+    InputField IDInputField;
+    InputField PWInputField;
+
+    void UserID(){
+        string ID = IDInputField.text;
+    }
+
+    void UserPW(){
+        string PW = PWInputField.text;
+    }
+    
+    
+
+
+}
